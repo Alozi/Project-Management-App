5d0f8b3ca6463274307d8bba7bfab013ba2c0f82
Alozi <Alozimozi@gmail.com>
2025-01-18 18:00:48 +0200

    Add components Aside and StartScreen

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+
+import Aside from "./components/Aside";
+import StartScreen from "./components/StartScreen";
+import FormCreateProject from "./components/FormCreateProject";
+
 function App() {
+  const [noProjects, setNoProjects] = useState(true);
+
   return (
     <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+      <main className="h-screen my-8 flex gap-8">
+        <Aside />
+        {noProjects && <StartScreen />}
+      </main>
     </>
   );
 }
